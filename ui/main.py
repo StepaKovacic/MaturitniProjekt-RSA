102ba3479b1d1924fc88e65764781f1c1615c281
StepaKovacic <stepan.kovacic@gmail.com>
2024-02-17 19:20:55 +0100

    flask interface and html generation

--- ui/main.py
+++ ui/main.py
@@ -3,9 +3,29 @@ from fileinput import filename
 from flask import *
 app = Flask(__name__) 
 
-@app.route('/') 
-def main(): 
-	return render_template("index.html") 
+# @app.route('/ne') 
+# def main(): 
+# 	return render_template("index.html") 
+
+@app.route("/desifrovani")
+def desifrovani():
+	return render_template("desifrovani.html")
+
+@app.route("/zasifrovani")
+def zasifrovani():
+	return render_template("sifrovani.html")
+
+@app.route("/generovani_klicu")
+def keygen():
+	return render_template("keygen.html")
+
+@app.route("/programatorska_dokumentace")
+def programatorska_dokumentace():
+	return render_template("programatorska_dokumentace.html")
+
+@app.route("/")
+def index():
+	return render_template("uzivatelska_dokumentace.html")
 
 @app.route('/uspech', methods = ['POST']) 
 def success(): 
