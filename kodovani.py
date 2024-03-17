6be9d6c4296da93db44854f732ce0fad2e4b011b
StepaKovacic <stepan.kovacic@gmail.com>
2024-03-17 13:03:56 +0100

    ten podelanej kod uz jsem opravil

--- kodovani.py
+++ kodovani.py
@@ -7,10 +7,13 @@ vsechny_znaky = " abcdefghijklmnopqrstuvwxyzáéíóůúýěčďňřšťž123456
 def enc(text_na_prevod, n, e):
     cisly_vyjadreny_text = text_na_cisla(text_na_prevod)
 
-    skok = 2 * (int(len(str(n))/2) - 1)
+    # skok = 2 * (int(len(str(n))/2) - 1)
+    # minimalni delka n je 7 a skok musi byt mensi nez n
+    skok = 6
 
     rozdeleny_ciselny_retezec = [(cisly_vyjadreny_text[i:i+skok]) for i in range(0, len(cisly_vyjadreny_text), skok)]
-    
+    # print(rozdeleny_ciselny_retezec) tohle asi nepotrebuju
+    # jen je videt ze posledni retezec je nejkratsi
     zasifrovany = []
     for clen_ciselneho_rezezce in rozdeleny_ciselny_retezec:
         zasifrovany.append((int(clen_ciselneho_rezezce)**e)%n)
@@ -24,6 +27,7 @@ def dec(zakodovany_text, n, d):
         #přidávám hodně nul, abych doplnil ty, které se v průběhu konveze str->int ztratí
         # dekodovany_text += (str(10000000 + gmpy2.powmod(cast_zakodovaneho_textu, d, n))[2:])
         dekodovany_text += (str(10000000 + gmpy2.powmod(cast_zakodovaneho_textu, d, n))[2:])
+        #tech nul se tam prida asi az moc
     
     return("".join([vsechny_znaky[kod] for kod in [int(dekodovany_text[int(i):int(i)+2]) for i in range(0, len(dekodovany_text), 2)]]))
     print(dekodovany_text)
@@ -31,12 +35,18 @@ def dec(zakodovany_text, n, d):
 
 def test(zprava):
     # print(zprava)
+    zprava = zprava + "dddddd"
     klic = generovani_klice()
-    print(zprava == dec(enc(zprava, klic["verejny_klic"][0], klic["verejny_klic"][1]), klic["soukromy_klic"][0], klic["soukromy_klic"][1]))
+    # print(zprava == dec(enc(zprava, klic["verejny_klic"][0], klic["verejny_klic"][1]), klic["soukromy_klic"][0], klic["soukromy_klic"][1]).replace("      ", ""))
+    # print(dec(enc(zprava, klic["verejny_klic"][0], klic["verejny_klic"][1]), klic["soukromy_klic"][0], klic["soukromy_klic"][1]).replace("dddddd", "") in zprava)
+    x = dec(enc(zprava, klic["verejny_klic"][0], klic["verejny_klic"][1]), klic["soukromy_klic"][0], klic["soukromy_klic"][1]).replace("dddddd", "")
+    # print(re.sub(x, "", zprava))
+    print("shoduje se původní zpráva s dekodovanou:" +  str(re.sub("dddddd", "", x ) == re.sub("dddddd", "", zprava)) )
+    print(str(re.sub("dddddd", "", x )), re.sub("dddddd", "", zprava))
+    # print(dec(enc(zprava, klic["verejny_klic"][0], klic["verejny_klic"][1]), klic["soukromy_klic"][0], klic["soukromy_klic"][1]))
 
 
 
 if __name__ == "__main__":
-    for i in range(10):
-        test("")
-    # returned "lorem ips um"
+    for i in range(100):
+        test(" adsf sdfsaf ")
