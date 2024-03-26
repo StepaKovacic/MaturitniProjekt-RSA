2f7f7dbbce8b569ea9202caacf1cd6bd97347f7c
StepaKovacic <stepan.kovacic@gmail.com>
2024-03-26 10:41:03 +0100

    cache

---
+++ utils.py
@@ -0,0 +1,77 @@
+import re
+import random
+
+vsechny_znaky = " abcdefghijklmnopqrstuvwxyzáéíóůúýěčďňřšťž1234567890.,"
+
+# def cisla_na_text():
+#     print("hello world")
+
+
+def text_na_cisla(reterez_na_prevod):
+        global vsechny_znaky
+        reterez_na_prevod = reterez_na_prevod.lower()
+        
+
+        for i in reterez_na_prevod:
+            if i not in vsechny_znaky:
+                print("toto nelze vykonat, v řetězci se nachází nedovolený znak: " + str(i))
+                return
+        vysledek = ""
+
+        for i in reterez_na_prevod:
+            vysledek += str(100 + (re.search(i, vsechny_znaky).start()))[1:]
+            
+        return(vysledek)
+
+def generovani_klice():
+    def Soudelna(cislo_a, cislo_b):
+        delitele_cisla_a = []
+        for i in range(1, cislo_a+1):
+            if cislo_a%i == 0:
+                delitele_cisla_a.append(i)
+        #print(delitele_cisla_a)
+        for i in delitele_cisla_a:
+            if cislo_b%i == 0 and i!=1:
+                return True 
+        return False
+
+
+    def udelej_velke_prvocislo():
+        while True:
+            x = random.randrange(1000, 10000)
+            is_prime = True
+            for i in range(2, int(x**0.5) + 1):
+                if x % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                return x
+
+
+    def Vygeneruj_klice(prvocislo_p, prvocislo_q):
+        n = prvocislo_p * prvocislo_q
+
+        #Eulerova funkce 
+        phi = (prvocislo_p - 1) * (prvocislo_q - 1)
+
+        #zvolíme náhodné e nesoudělné s phi
+        while True:
+            # vygenerování náhodného čísla e které je nesoudělné s phi
+            e = random.randrange(1000, 10000)
+            if Soudelna(e, phi) == False:
+                break
+        
+        d = 1
+        while True:
+            
+            
+            if (d*e)%phi == 1:
+                break
+            else: 
+                d += 1
+        return {"verejny_klic":(n, e), "soukromy_klic":(n, d) }
+    return Vygeneruj_klice(udelej_velke_prvocislo(), udelej_velke_prvocislo())
+
+
+if __name__ == "__main__":
+    print(generovani_klice())
