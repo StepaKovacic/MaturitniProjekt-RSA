83ad3a540af17fb01e8c9e21d8eab60d63cb683e
StepaKovacic <stepan.kovacic@gmail.com>
2024-04-20 14:19:45 +0200

    uhlazovani kodu

--- kodovani.py
+++ kodovani.py
@@ -1,15 +1,10 @@
 from utils import *
 
-#chci retezec uchovat v ramci kodu jen na jednom miste
 vsechny_znaky = znaky()
 
 def enc(text_na_prevod, n, e):
-    cisly_vyjadreny_text = text_na_cisla(text_na_prevod)
-
     skok = 6
-
-    rozdeleny_ciselny_retezec = [(cisly_vyjadreny_text[i:i+skok]) for i in range(0, len(cisly_vyjadreny_text), skok)]
-
+    rozdeleny_ciselny_retezec = text_na_cisla(text_na_prevod, skok)
     list_zasifrovanych_substringu = []
     for clen_ciselneho_rezezce in rozdeleny_ciselny_retezec:
         list_zasifrovanych_substringu.append((int(clen_ciselneho_rezezce)**e)%n)
@@ -25,13 +20,11 @@ def dec(zakodovany_text, n, d):
             result *= base
         return result % mod
     dekodovany_text = ""
-
     for cast_zakodovaneho_textu in zakodovany_text:
         #přidávám hodně nul, abych doplnil ty, které se v průběhu konveze str->int ztratí
         dekodovany_text += (str(10000000 + powmod(cast_zakodovaneho_textu, d, n))[2:])
 
     return cisla_na_text(dekodovany_text)
-
 
 def test(zprava):
     red_zprava = zprava
@@ -39,14 +32,9 @@ def test(zprava):
     zprava = zprava + vata
     klic = generovani_klice()
     x = dec(enc(zprava, klic["verejny_klic"][0], klic["verejny_klic"][1]), klic["soukromy_klic"][0], klic["soukromy_klic"][1])
-    
     string = list(x[::-1])
-    
     for index, item in enumerate(string):
-        y = len(vata)
-        
         if len(vata)>0:
-            
             if item in vata:
                 vata = vata.replace(item, "", 1)
             string[index] = ""
@@ -56,12 +44,11 @@ def test(zprava):
     return str("".join(string)[::-1] == red_zprava) 
 
 if __name__ == "__main__":
-
     test_usepch_bool = True
     for i in range(10):
         if test(vsechny_znaky) == False:
             test_usepch_bool = False
             break
-    print("Byl test úspěšný? " + str(test_usepch_bool).upper())
+    print("Byl test úspěšný? " + str(test_usepch_bool))
             
             
--- utils.py
+++ utils.py
@@ -8,15 +8,13 @@ def znaky():
 
 vsechny_znaky = znaky()
 
-def text_na_cisla(reterez_na_prevod):
+def text_na_cisla(reterez_na_prevod, skok):
         """
         Funkce převede text na list celých čísel
 
         Vstup -> text, délka celých čísel (skok)
 
         Výstup -> list celých čísel o délce "skok"
-        
-        Funkce dostane text a převede jej na list optimálně dlouhých celých čísel (integerů)
         """
         global vsechny_znaky
         
@@ -25,12 +23,13 @@ def text_na_cisla(reterez_na_prevod):
             if i not in vsechny_znaky:
                 print("toto nelze vykonat, v řetězci se nachází nedovolený znak: " + str(i))
                 return
-        vysledek = ""
+        nerozdeleny_text = ""
 
         for i in reterez_na_prevod:
-            vysledek += str(100 + (vsechny_znaky.find(i)))[1:]
+            nerozdeleny_text += str(100 + (vsechny_znaky.find(i)))[1:]
             
-        return(vysledek)
+        rozdeleny_retezec = [(nerozdeleny_text[i:i+skok]) for i in range(0, len(nerozdeleny_text), skok)]
+        return rozdeleny_retezec
 
 def cisla_na_text(cisla_na_prevod):
     """
@@ -50,7 +49,7 @@ def generovani_klice():
     
     Výstup -> ve formátu {"verejny_klic":(n, e), "soukromy_klic":(n, d) }
     """
-    def Soudelna(cislo_a, cislo_b):
+    def soudelna(cislo_a, cislo_b):
         delitele_cisla_a = []
         for i in range(1, cislo_a+1):
             if cislo_a%i == 0:
@@ -84,7 +83,7 @@ def generovani_klice():
     while True:
         # vygenerování náhodného čísla e které je nesoudělné s phi
         e = random.randrange(1000, 10000)
-        if Soudelna(e, phi) == False:
+        if soudelna(e, phi) == False:
             break
         
     d = 1
